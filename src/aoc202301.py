756c73c4aff2c8e7f907155ab64b986b1387a4d4
Jamie Murphy <git@jam.ie>
2023-12-04 20:23:44 +0000

    initial version

---
+++ src/aoc202301.py
@@ -0,0 +1,35 @@
+import pathlib
+import sys
+import json
+
+
+def parse(puzzle_input):
+    """Parse input."""
+    return puzzle_input.split()
+
+
+def part1(puzzle_input):
+    """Solve part 1."""
+    print(json.dumps(puzzle_input))
+    output = []
+    for line in puzzle_input:
+        first_num = None
+        last_num = None
+        for character in line:
+            if character.isdigit():
+                if not first_num:
+                    first_num = character
+                last_num = character
+        calculated_num = f"{first_num}{last_num}"
+        output.append(int(calculated_num))
+        print(calculated_num)
+    print(f"total: {sum(output)}")
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"\n{path}:")
+        file1 = open(path, "r")
+        lines = file1.read().splitlines()
+
+        print(part1(lines))
